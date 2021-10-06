58084fe12d78951cb9700c71ba7b9807f7881908
Isaque Almeida <isaque.337dd@gmail.com>
2021-10-06 10:55:27 -0300

    Add files

---
+++ script.js
@@ -0,0 +1,28 @@
+function pointer(element, value) {
+
+    element.style.setProperty('--rotation', value * 360)
+    console.log(value)
+
+}
+
+function clock() {
+
+    let hour = document.querySelector('[data-hour-hand]');
+    let minute = document.querySelector('[data-minute-hand]');
+    let second = document.querySelector('[data-second-hand]');
+
+    const currentDate = new Date();
+
+    const seconds = currentDate.getSeconds() / 60;
+    const minutes = (seconds + currentDate.getMinutes()) / 60;
+    const hours = (minutes + currentDate.getHours()) / 12;
+
+    pointer(second, seconds);
+    pointer(minute, minutes);
+    pointer(hour, hours);
+
+}
+
+setInterval(() => {
+    clock();
+}, 1000);
